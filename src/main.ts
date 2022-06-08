2b73088b478d6f9b817fd5fdce65620b20afe23a
dev-jb <bajracharyajenish@gmail.com>
2022-06-08 13:33:49 +0545

    [KT-1]: Initial commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { json, urlencoded } from 'express';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.use(helmet());
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+
+  const PORT = process.env.APP_PORT || 5004;
+  await app.listen(PORT || 5004);
+  console.info(
+    'Kilroy Blockchain Connector',
+    `Server running on 🚀 http://localhost:${PORT}`,
+  );
+}
+bootstrap();
