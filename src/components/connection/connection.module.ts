52ae883bb541f4e58fdaf056ce98a66ae7f1382f
dev-jb <bajracharyajenish@gmail.com>
2022-06-29 13:45:30 +0545

    [BB-50]: Add Blockchain Status Guard on check connection API

--- src/components/connection/connection.module.ts
+++ src/components/connection/connection.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { BcUserModule } from '../blockchain/bc-user/bc-user.module';
 import { ConnectionController } from './connection.controller';
 
 @Module({
+  imports: [BcUserModule],
   controllers: [ConnectionController],
 })
 export class ConnectionModule {}
