9b18e7eff49fcdab192cf0da85e738266a33d7d4
dev-jb <bajracharyajenish@gmail.com>
2022-08-17 14:16:30 +0545

    [BB-126]: Remove BlockchainStatusGuard from Connection API

--- src/components/connection/connection.controller.ts
+++ src/components/connection/connection.controller.ts
@@ -6,13 +6,12 @@ import {
   Get,
 } from '@nestjs/common';
 import { AuthorizationGuard } from 'src/@core/auth/guards/authorization.guard';
-import { BlockchainStatusGuard } from 'src/@core/auth/guards/blockchain-status.guard';
 import { Response } from 'src/@core/common/dto/response.dto';
 
 @Controller('connection')
 export class ConnectionController {
   @Get()
-  @UseGuards(BlockchainStatusGuard, AuthorizationGuard)
+  @UseGuards(AuthorizationGuard)
   @HttpCode(HttpStatus.OK)
   async checkConnection(): Promise<Response> {
     return new Response('OK', true).setStatusCode(HttpStatus.OK);
